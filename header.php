2fa33a2f4ed539137363936437e1fbab91117bca
Julian <Cuttinoj@go.stockton.edu>
2021-01-04 21:20:21 -0500

    CSS

--- header.php
+++ header.php
@@ -2,16 +2,18 @@
 <?php include_once 'isConnected.php';?>
 <html>
 <link href='Style/style.css' rel='stylesheet' type='text/css' media='all'>
-<body id="header">
-    <nav id="menu">
+<body>
+<div id="header">
     <h2>Clean Paws and All</h2>
+    <nav>
     <ul>
         <li><a href='home.php'>Home</a></li>
         <li><a href='clients.php'>Clients</a></li>
         <li><a href='finances.php'>Finance</a></li>
-        <li><a href='logout.php'>Logout</a></li>
-        <li><?php echo $ret['Name'];?></li>
+     	<li><?php echo $ret['Name'];?></li>
     </ul>
     </nav>
+    	<a href="logout.php"><button>Logout</button></a>
+    </div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,19 +1,25 @@
 <!DOCTYPE html>
 <html>
 <link href='Style/style.css' rel='stylesheet' type='text/css' media='all'>
-<h2>Login</h2>
+<h2 class="center">Login</h2>
+<div class="wrapper">
+
 <form method='post' action='login.php'>
-<div class='imgcontainer'>
+<div>
     <img src='images\logo.png'>
 </div>
 
-<div class ='container'>
+<div class="sign-in">
+<div class="input-fields">
     <label for ='username'><b>Username</b></label>
-    <input type = 'text' placeholder='Enter username' name='username' required>
+    <input type = 'text' class="input"placeholder='Enter username' name='username' required>
 
     <label for='password'><b>Password</b></label>
-    <input type='password' placeholder='Enter password' name='password' required>
-
+    <input type='password'class="input" placeholder='Enter password' name='password' required>
     <button type='submit'>Login</button>
-            
+</div>
+    
+ </div>
+ </form>
+</div>
 </html>
